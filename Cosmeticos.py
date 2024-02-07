6fc6e951c5b5aed47166189e990c54837808e69a
Michele Rodrigues <154391130+MicheleRodriguesc@users.noreply.github.com>
2024-02-07 17:15:14 -0300

    Add files via upload

---
+++ Cosmeticos.py
@@ -0,0 +1,7 @@
+# Definindo a lista de produtos
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+# Imprimindo cada produto com uma frase personalizada
+for produto in lista_produtos:
+    frase = f"Temos {produto} à venda!"
+    print(frase)
